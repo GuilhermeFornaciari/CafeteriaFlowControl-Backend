72185145fe3b22ed5b27d35fc286bf9b016a81c5
Júlio <jcaguiarpg@gmail.com>
2024-01-22 12:53:18 -0400

    Adição da estrutura de pastas e da conexão com a database

---
+++ src/infra/http/nest/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { databaseProviders as connectDatabase } from './mongodbProvider';
+@Module({
+    controllers: [
+        
+    ],
+    providers: [
+        connectDatabase,
+    ],
+    exports: [connectDatabase]
+})
+export class AppModule {}
