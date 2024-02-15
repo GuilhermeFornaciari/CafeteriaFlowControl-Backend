4af7b8961eb353d1734ddd9b552e4597be170362
Júlio <jcaguiarpg@gmail.com>
2024-02-15 19:56:11 -0400

    deploy acertado

--- src/infra/http/nest/mongodbProvider.ts
+++ src/infra/http/nest/mongodbProvider.ts
@@ -4,6 +4,6 @@ export const databaseProviders = {
   provide: 'DATABASE_CONNECTION',
   useFactory: (): Promise<typeof mongoose> => {
     config();
-    return mongoose.connect('mongodb+srv://jcaguiarpg:AtwqnpnWrBt2kc0p@teting.j3va5y2.mongodb.net/?retryWrites=true&w=majority');
+    return mongoose.connect(process.env.connectionString);
   },
 };
