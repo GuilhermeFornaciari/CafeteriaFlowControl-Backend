21fb8e1f9862e39141cdeb73a873da01fe7473eb
Júlio <jcaguiarpg@gmail.com>
2024-03-18 23:04:52 -0400

    testes de caso de uso, updateAll feito

--- src/application/repository/QueueRepositoryInterface.ts
+++ src/application/repository/QueueRepositoryInterface.ts
@@ -4,6 +4,6 @@ export default interface QueueRepositoryInterface {
     save(queue: Omit<Queue, 'id'>): Promise<Queue>,
     getAll(organizationId: String): Promise<Array<Queue>>,
     getOne(id: String): Promise<Queue>,
-    updateAll(sequence: Array<String>, id: string): Promise<void>,
+    updateAll(sequence: Array<String>, id: String): Promise<void>,
     delete(id: String): Promise<void>,
 }
