b06401973711d7ac5a5bb55a7e44f7a675c7794a
Júlio <jcaguiarpg@gmail.com>
2024-04-02 15:44:51 -0400

    quick fix

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './src/infra/http/nest/app.module';
 
 async function main(port: number) {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: true,
-    credentials: true, // se tu for ter credenciais no cabecalho
+  await app.enableCors({
+    origin: '*', // permite qualquer origem
+    credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders:
-      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+    allowedHeaders: ['Content-Type', 'Accept', 'authorization'],
   });
   await app.listen(port);
 }
+
 const port = 4000;
-main(port); 
+main(port);
