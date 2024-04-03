ad9d2d5652dc8f8fd0b01311ec3442438474dde4
Guilherme de Souza Fornaciari <fornaciari049@gmail.com>
2024-04-03 20:20:18 -0300

    teste resolver cors 5

--- main.ts
+++ main.ts
@@ -5,9 +5,19 @@ async function main(port: number) {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://ur-flow.vercel.app/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['content-type', 'Access-Control-Allow-Origin'],
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // allowed headers
+    allowedHeaders: [
+      'Content-Type',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Authorization',
+    ],
+    // headers exposed to the client
+    exposedHeaders: ['Authorization'],
+    credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
   });
   await app.listen(port);
 }
