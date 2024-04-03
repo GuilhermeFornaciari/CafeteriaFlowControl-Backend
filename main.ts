c0bd0c15d6aa9e0652248bb96c47e9f729bcec56
Guilherme de Souza Fornaciari <fornaciari049@gmail.com>
2024-04-03 20:04:57 -0300

    teste resolver cors 1

--- main.ts
+++ main.ts
@@ -4,15 +4,9 @@ import { AppModule } from './src/infra/http/nest/app.module';
 async function main(port: number) {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:4200',
     credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Access-Control-Allow-Origin',
-    ],
   });
   await app.listen(port);
 }
