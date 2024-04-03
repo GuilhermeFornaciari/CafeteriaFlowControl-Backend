4b782c9d79690f40d7f560eadf68ae086f326652
Guilherme de Souza Fornaciari <fornaciari049@gmail.com>
2024-04-03 20:21:17 -0300

    teste resolver cors 6

--- main.ts
+++ main.ts
@@ -6,18 +6,6 @@ async function main(port: number) {
 
   app.enableCors({
     origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowed headers
-    allowedHeaders: [
-      'Content-Type',
-      'Origin',
-      'X-Requested-With',
-      'Accept',
-      'Authorization',
-    ],
-    // headers exposed to the client
-    exposedHeaders: ['Authorization'],
-    credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
   });
   await app.listen(port);
 }
