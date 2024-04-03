dc6f1a18bcee52137d665174856a2dc124d1baed
Guilherme de Souza Fornaciari <fornaciari049@gmail.com>
2024-04-03 20:03:15 -0300

    alterei os headers permitidos

--- main.ts
+++ main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/infra/http/nest/app.module';
 
 async function main(port: number) {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders:
-      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+    allowedHeaders: [
+      'Content-Type',
+      'Accept',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+    ],
   });
   await app.listen(port);
 }
